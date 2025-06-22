23ee261ab5a3a7c90def6132c1a9ce58ff60b57c
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2025-06-22 13:34:57 -0300

    refactor: remove console log and change logo

--- src/app/app.ts
+++ src/app/app.ts
@@ -23,6 +23,5 @@ export class App implements OnInit {
     this.certificadoService.certificados = certificados
       ? JSON.parse(certificados)
       : [];
-    console.log(this.certificadoService.certificados);
   }
 }
--- src/app/pages/certificados/certificados.ts
+++ src/app/pages/certificados/certificados.ts
@@ -18,6 +18,5 @@ export class Certificados implements OnInit {
 
   ngOnInit(): void {
     this.certificados = this.certificadoService.certificados;
-    console.log(this.certificados);
   }
 }
